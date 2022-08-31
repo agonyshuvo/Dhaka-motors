4bf47aebea0155341e3e75a0565bf4110816a537
Shuvo Khan <agonysuvo@gmail.com>
2022-08-31 17:22:55 +0600

    31 Aug

--- pages/Manager/layout/head.php
+++ pages/Manager/layout/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -44,8 +45,7 @@
     <![endif]-->
 
     <!-- Google Font -->
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
 
 
@@ -61,148 +61,151 @@
 
 
 </head>
+
 <body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
+    <div class="wrapper">
 
-    <header class="main-header">
-        <!-- Logo -->
-        <a href="index.php" class="logo">
-            <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>D</b>Mt</span>
-            <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Dhaka</b>Motors</span>
-        </a>
-        <!-- Header Navbar: style can be found in header.less -->
-        <nav class="navbar navbar-static-top">
-            <!-- Sidebar toggle button-->
-            <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
-                <span class="sr-only">Toggle navigation</span>
+        <header class="main-header">
+            <!-- Logo -->
+            <a href="index.php" class="logo">
+                <!-- mini logo for sidebar mini 50x50 pixels -->
+                <span class="logo-mini"><b>D</b>Mt</span>
+                <!-- logo for regular state and mobile devices -->
+                <span class="logo-lg"><b>Dhaka</b>Motors</span>
             </a>
+            <!-- Header Navbar: style can be found in header.less -->
+            <nav class="navbar navbar-static-top">
+                <!-- Sidebar toggle button-->
+                <a href="#" class="sidebar-toggle" data-toggle="push-menu" role="button">
+                    <span class="sr-only">Toggle navigation</span>
+                </a>
 
-            <div class="navbar-custom-menu">
-                <ul class="nav navbar-nav">
-                    <!-- Messages: style can be found in dropdown.less-->
-                    <li class="dropdown user user-menu">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <img src="../../dist/img/avatar5.png" class="user-image" alt="User Image">
-                            <span class="hidden-xs">
-              <?php
-              if(logged_in()==true){
-                  echo($_SESSION['userName']);
-              }else{
-                  echo "No User";
-              }
-              ?>
-              </span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <!-- User image -->
-                            <li class="user-header">
-                                <img src="../../dist/img/avatar5.png" class="img-circle" alt="User Image">
-
-                                <p>
+                <div class="navbar-custom-menu">
+                    <ul class="nav navbar-nav">
+                        <!-- Messages: style can be found in dropdown.less-->
+                        <li class="dropdown user user-menu">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                                <img src="https://pbs.twimg.com/profile_images/1437631089229500420/panTZz-J_400x400.jpg" class="user-image" alt="User Image">
+                                <span class="hidden-xs">
                                     <?php
-                                    if(logged_in()==true){
-                                        echo($_SESSION['userName']);
-                                    }else{
+                                    if (logged_in() == true) {
+                                        echo ($_SESSION['userName']);
+                                    } else {
                                         echo "No User";
                                     }
                                     ?>
-                                    <small>Member since Nov. 2012</small>
-                                </p>
-                            </li>
-                            <!-- Menu Body -->
-                            <li class="user-body">
-                                <div class="row">
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Followers</a>
+                                </span>
+                            </a>
+                            <ul class="dropdown-menu">
+                                <!-- User image -->
+                                <li class="user-header">
+                                    <!-- <img src="../../dist/img/avatar5.png" class="img-circle" alt="User Image"> -->
+
+                                    <img src="https://pbs.twimg.com/profile_images/1437631089229500420/panTZz-J_400x400.jpg" class="img-circle" alt="User Image">
+
+                                    <p>
+                                        <?php
+                                        if (logged_in() == true) {
+                                            echo ($_SESSION['userName']);
+                                        } else {
+                                            echo "No User";
+                                        }
+                                        ?>
+                                        <small>Member since Aug. 2022</small>
+                                    </p>
+                                </li>
+                                <!-- Menu Body -->
+                                <li class="user-body">
+                                    <div class="row">
+                                        <div class="col-xs-4 text-center">
+                                            <a href="#">Follower : 9K</a>
+                                        </div>
+                                        <div class="col-xs-4 text-center">
+                                            <a href="#">Sales: 10 </a>
+                                        </div>
+                                        <div class="col-xs-4 text-center">
+                                            <a href="#">Friends:0</a>
+                                        </div>
                                     </div>
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Sales</a>
+                                    <!-- /.row -->
+                                </li>
+                                <!-- Menu Footer-->
+                                <li class="user-footer">
+                                    <div class="pull-left">
+                                        <a href="https://bcspoint.net/mrkhan/" target="blank" class="btn btn-default btn-flat">Profile</a>
                                     </div>
-                                    <div class="col-xs-4 text-center">
-                                        <a href="#">Friends</a>
+                                    <div class="pull-right">
+                                        <a href="../logout.php" class="btn btn-default btn-flat">Sign out</a>
                                     </div>
-                                </div>
-                                <!-- /.row -->
-                            </li>
-                            <!-- Menu Footer-->
-                            <li class="user-footer">
-                                <div class="pull-left">
-                                    <a href="#" class="btn btn-default btn-flat">Profile</a>
-                                </div>
-                                <div class="pull-right">
-                                    <a href="../logout.php" class="btn btn-default btn-flat">Sign out</a>
-                                </div>
-                            </li>
+                                </li>
+                            </ul>
+                        </li>
+
+                    </ul>
+                </div>
+            </nav>
+        </header>
+        <!-- Left side column. contains the logo and sidebar -->
+        <aside class="main-sidebar">
+            <!-- sidebar: style can be found in sidebar.less -->
+            <section class="sidebar">
+                <!-- Sidebar user panel -->
+
+                <ul class="sidebar-menu" data-widget="tree">
+                    <li class="header"></li>
+
+                    <li><a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-cogs"></i>
+                            <span>Sales</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="sale_motorcycle.php"><i class="fa fa-circle-o"></i> Motorcycle</a></li>
+                            <li><a href="sale_parts.php"><i class="fa fa-circle-o"></i> Parts</a></li>
+                            <li><a href="sale_list.php"><i class="fa fa-circle-o"></i> Sold Motorcycles</a></li>
+                            <li><a href="sale_parts_list.php"><i class="fa fa-circle-o"></i> Sold Parts</a></li>
                         </ul>
                     </li>
 
+                    <!-- <li><a href="service.php"><i class="fa fa-book"></i> <span>Service</span></a></li> -->
+                    <!-- <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-wrench"></i>
+                            <span>Service</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="service.php"><i class="fa fa-circle-o"></i> Free Service</a></li>
+                            <li><a href="paid_service.php"><i class="fa fa-circle-o"></i> Paid Service</a></li>
+                             <li><a href="pre_book_sale_list.php"><i class="fa fa-circle-o"></i> Pre-Book Sales</a></li>
+
                 </ul>
-            </div>
-        </nav>
-    </header>
-    <!-- Left side column. contains the logo and sidebar -->
-    <aside class="main-sidebar">
-        <!-- sidebar: style can be found in sidebar.less -->
-        <section class="sidebar">
-            <!-- Sidebar user panel -->
+                </li> -->
+                    <li class="treeview">
+                        <a href="#">
+                            <i class="fa fa-check-square"></i>
+                            <span>Pre-Book</span>
+                            <span class="pull-right-container">
+                                <i class="fa fa-angle-left pull-right"></i>
+                            </span>
+                        </a>
+                        <ul class="treeview-menu">
+                            <li><a href="pre_book.php"><i class="fa fa-circle-o"></i> Pre-Booking</a></li>
+                            <li><a href="pre_book_list.php"><i class="fa fa-circle-o"></i> Pre-Book List</a></li>
+                            <li><a href="pre_book_sale_list.php"><i class="fa fa-circle-o"></i> Pre-Book Sales</a></li>
 
-            <ul class="sidebar-menu" data-widget="tree">
-                <li class="header"></li>
+                        </ul>
+                    </li>
+                    <li><a href="distribute.php"><i class="fa fa-sitemap"></i> <span>Distribution</span></a></li>
 
-                <li><a href="index.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
-
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-cogs"></i>
-                        <span>Sales</span>
-                        <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="sale_motorcycle.php"><i class="fa fa-circle-o"></i> Motorcycle</a></li>
-                        <li><a href="sale_parts.php"><i class="fa fa-circle-o"></i> Parts</a></li>
-                        <li><a href="sale_list.php"><i class="fa fa-circle-o"></i> Sold Motorcycles</a></li>
-                        <li><a href="sale_parts_list.php"><i class="fa fa-circle-o"></i> Sold Parts</a></li>
-                    </ul>
-                </li>
-
-                <!-- <li><a href="service.php"><i class="fa fa-book"></i> <span>Service</span></a></li> -->
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-wrench"></i>
-                        <span>Service</span>
-                        <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="service.php"><i class="fa fa-circle-o"></i> Free Service</a></li>
-                        <li><a href="paid_service.php"><i class="fa fa-circle-o"></i> Paid Service</a></li>
-                        <!-- <li><a href="pre_book_sale_list.php"><i class="fa fa-circle-o"></i> Pre-Book Sales</a></li> -->
-
-                    </ul>
-                </li>
-                <li class="treeview">
-                    <a href="#">
-                        <i class="fa fa-check-square"></i>
-                        <span>Pre-Book</span>
-                        <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
-                    </a>
-                    <ul class="treeview-menu">
-                        <li><a href="pre_book.php"><i class="fa fa-circle-o"></i> Pre-Booking</a></li>
-                        <li><a href="pre_book_list.php"><i class="fa fa-circle-o"></i> Pre-Book List</a></li>
-                        <li><a href="pre_book_sale_list.php"><i class="fa fa-circle-o"></i> Pre-Book Sales</a></li>
-
-                    </ul>
-                </li>
-                <li><a href="distribute.php"><i class="fa fa-sitemap"></i> <span>Distribution</span></a></li>
-
-            </ul>
-        </section>
-        <!-- /.sidebar -->
-    </aside>
+                </ul>
+            </section>
+            <!-- /.sidebar -->
+        </aside>
