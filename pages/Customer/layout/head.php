56998e573d4e53cc0d7a2ed9777f8200f2bb8520
Shuvo Khan <agonysuvo@gmail.com>
2022-09-08 15:06:58 +0600

    Last

--- pages/Customer/layout/head.php
+++ pages/Customer/layout/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html>
+
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -28,121 +29,121 @@
   <!-- Google Font -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
-<?php
-	session_start();
-	$connection = mysqli_connect("localhost", "root", "", "dhaka_motors");
-	include '../functions.php';
+  <?php
+  session_start();
+  $connection = mysqli_connect("localhost", "root", "", "dhaka_motors");
+  include '../functions.php';
   protect_page();
   protect_page_customer();
-  
 
-  
-		$query="select * 
+
+
+  $query = "select * 
 				from customer 
-				where phone='".$_SESSION['userName']."'";
-        $result=mysqli_query($connection,$query);
-        if($result){
-			
-          $row=mysqli_fetch_array($result,MYSQLI_ASSOC);
-        $name = $row['name'];
-        $phone = $row['phone'];
-        }
-?>
+				where phone='" . $_SESSION['userName'] . "'";
+  $result = mysqli_query($connection, $query);
+  if ($result) {
+
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+    $name = $row['name'];
+    $phone = $row['phone'];
+  }
+  ?>
 
 </head>
 <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+
 <body class="hold-transition skin-black layout-top-nav">
 
-<div class="wrapper">
+  <div class="wrapper">
 
-  <header class="main-header">
-    <nav class="navbar navbar-static-top">
-      <div class="container">
-        <div class="navbar-header">
-          <a href="index.php" class="navbar-brand"><b>Dhaka</b>Motors</a>
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
-            <i class="fa fa-bars"></i>
-          </button>
-        </div>
+    <header class="main-header">
+      <nav class="navbar navbar-static-top">
+        <div class="container">
+          <div class="navbar-header">
+            <a href="index.php" class="navbar-brand"><b>Dhaka</b>Motors</a>
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
+              <i class="fa fa-bars"></i>
+            </button>
+          </div>
 
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
-          <ul class="nav navbar-nav">
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Own information <span class="caret"></span></a>
-              <ul class="dropdown-menu" role="menu">
-                <li><a href="own_motorcycle.php">My Bike</a></li>
-                <li><a href="own_booking.php">My Booking</a></li>
-                <!-- <li><a href="#">Something else here</a></li>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+          <div class="collapse navbar-collapse pull-left" id="navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">Own information <span class="caret"></span></a>
+                <ul class="dropdown-menu" role="menu">
+                  <li><a href="own_motorcycle.php">My Bike</a></li>
+                  <li><a href="own_booking.php">My Booking</a></li>
+                  <!-- <li><a href="#">Something else here</a></li>
                 <li class="divider"></li>
                 <li><a href="#">Separated link</a></li>
                 <li class="divider"></li>
                 <li><a href="#">One more separated link</a></li> -->
-              </ul>
-            </li>
-            <li><a href="info.php">Info <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Help</a></li>
-            <li><a href="#">About DM</a></li>
-          </ul>
-          <form class="navbar-form navbar-left" role="search">
-            <div class="form-group">
-              <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
-            </div>
-          </form>
-        </div>
-        <!-- /.navbar-collapse -->
-        <!-- Navbar Right Menu -->
-        <div class="navbar-custom-menu">
-          <ul class="nav navbar-nav">
-            <!-- Messages: style can be found in dropdown.less-->
-            
-            <li class="dropdown user user-menu">
-            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="../../dist/img/avatar5.png" class="user-image" alt="User Image">
-              <span class="hidden-xs">
-              <?php 
-              
-							  echo $name; 
-              
-               
-             
-              ?>
-              </span>
-            </a>
-            <ul class="dropdown-menu">
-              <!-- User image -->
-              <li class="user-header">
-                <img src="../../dist/img/avatar5.png" class="img-circle" alt="User Image">
-
-                <p>
-                <?php 
-                   echo $phone; 
-                ?>
-                  <small>Customer</small>
-                </p>
+                </ul>
               </li>
-              <!-- Menu Body -->
-              <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-6 text-center">
-                    <a href="../change_password.php">Change Password</a>
-                  </div>
-                  <div class="col-xs-2 text-center">
-                    <!-- <a href="#">Sales</a> -->
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="../logout.php">Sign out</a>
-                  </div>
-                </div>
-                <!-- /.row -->
-              </li>
-          </ul>
-        </div>
-        <!-- /.navbar-custom-menu -->
-      </div>
-      <!-- /.container-fluid -->
-    </nav>
-  </header>
-  <!-- Full Width Column -->
-  <div class="content-wrapper">
+              <li><a href="info.php">Info <span class="sr-only">(current)</span></a></li>
+              <li><a href="#">Help</a></li>
+              <li><a href="#">About DM</a></li>
+            </ul>
+            <form class="navbar-form navbar-left" role="search">
+              <div class="form-group">
+                <input type="text" class="form-control" id="navbar-search-input" placeholder="Search">
+              </div>
+            </form>
+          </div>
+          <!-- /.navbar-collapse -->
+          <!-- Navbar Right Menu -->
+          <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+              <!-- Messages: style can be found in dropdown.less-->
 
+              <li class="dropdown user user-menu">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                  <img src="https://pbs.twimg.com/media/Elh4EQ0UYAAi0lw.jpg" class="user-image" alt="User Image">
+                  <span class="hidden-xs">
+                    <?php
+
+                    echo $name;
+
+
+
+                    ?>
+                  </span>
+                </a>
+                <ul class="dropdown-menu">
+                  <!-- User image -->
+                  <li class="user-header">
+                    <img src="https://pbs.twimg.com/media/Elh4EQ0UYAAi0lw.jpg" class="img-circle" alt="User Image">
+
+                    <p>
+                      <?php
+                      echo $phone;
+                      ?>
+                      <small>Customer</small>
+                    </p>
+                  </li>
+                  <!-- Menu Body -->
+                  <li class="user-body">
+                    <div class="row">
+                      <div class="col-xs-6 text-center">
+                        <a href="../change_password.php">Change Password</a>
+                      </div>
+                      <div class="col-xs-2 text-center">
+                        <!-- <a href="#">Sales</a> -->
+                      </div>
+                      <div class="col-xs-4 text-center">
+                        <a href="../logout.php">Sign out</a>
+                      </div>
+                    </div>
+                    <!-- /.row -->
+                  </li>
+                </ul>
+          </div>
+          <!-- /.navbar-custom-menu -->
+        </div>
+        <!-- /.container-fluid -->
+      </nav>
+    </header>
+    <!-- Full Width Column -->
+    <div class="content-wrapper">
